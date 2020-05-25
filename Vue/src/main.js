95e0d71c26318a5d3f46b11bd91cc3a9c140215e
HolmeCat <moonwindfu@126.com>
2020-05-25 20:34:29 +0800

    Vue这次是正式部署完毕

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Element from 'element-ui'
+
 import router from './plugins/router.js'
-import './plugins/element.js'
+import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios,axios)
+Vue.use(Element)
 
 new Vue({
   router,
